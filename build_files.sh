5bb742c54b425a6d8af8489a5bf6b130d5022dda
Yakesh70 <syakesh82@gmail.com>
2025-09-16 19:44:18 +0530

    Optimize for Vercel deployment - reduce package sizes

---
+++ build_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build script for Vercel
+echo "Building Django project..."
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
--- normality_project/settings.py
+++ normality_project/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles_build" / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
